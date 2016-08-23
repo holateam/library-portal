72a3c98d040e81c3fc0d520f79655b77f517f698
serhii-kobzin <serhii-kobzin@gmail.com>
2016-08-23 13:22:49 +0300

    create sidebar.js

---
+++ public/js/sidebar.js
@@ -0,0 +1,11 @@
+$('#filter_new').click(function () {
+    doAjaxQuery();
+});
+
+$('#filter_popular').click(function () {
+    //doAjaxQuery();
+});
+
+$('#filter_all').click(function () {
+    //doAjaxQuery();
+});
