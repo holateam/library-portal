f1c6badd17ac91cac5cc179f8531f20bd4b10f05
lex <info.tehnokot@gmail.com>
2016-08-23 20:07:36 +0300

    modified routes/book.js

--- routes/book.js
+++ routes/book.js
@@ -3,7 +3,20 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('book', { title: 'Express' });
+  res.render('book',
+  { success:'true',
+    data: {
+      id:'136364831',
+      title: 'Вэб-разработка с применением Node и Express. Полноценное использование стека JavaScript',
+      authors: 'Браун И.',
+      description: 'описание книги',
+      busy: 'false',
+      year: '2015',
+      new: 'true',
+      pages: '444'
+     }
+   });
+   console.log(res);
 });
 
 module.exports = router;
