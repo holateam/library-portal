3a9a574c1bd3e63e1a86bb462e2594a983fc52d4
serhii-kobzin <serhii-kobzin@gmail.com>
2016-08-24 12:52:04 +0300

    fix index.jade

--- public/js/sidebar.js
+++ public/js/sidebar.js
@@ -1,12 +1,13 @@
 var sidebarItems = $('#sidebar li');
 sidebarItems.click(function () {
     var _this = $(this);
-    sidebarItems.removeClass("active");
-    _this.closest('li').addClass("active");
-    controller.doAjaxQuery('GET', '/api/v1/books?filter=' + _this.attr('data-filter'), null, function (err, data) {
-        if (err) {
-            throw err;
-        }
-        view.addBooksItems(data.books);
-    });
+    sidebarItems.removeClass('active');
+    _this.closest('li').addClass('active');
+    // controller.doAjaxQuery('GET', '/api/v1/books?filter=' + _this.attr('data-filter'), null, function (err, data) {
+    //     if (err) {
+    //         throw err;
+    //     }
+    //     view.addBooksItems(data.books);
+    // });
+    view.addBookItem();
 });
