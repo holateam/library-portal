81ab993b81975a0cd98b70196e073f756dc13e4a
Dmitriy Malyarenko <dmitriy.malyarenko@gmail.com>
2016-08-24 13:04:01 +0300

    modified routes/index.js

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/book', function(req, res, next) {
+  res.render('book', { title: 'Book' });
+});
+
 module.exports = router;
