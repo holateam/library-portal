55ce68054abc4e3cc31b091f15c982b5707fbec9
lex <info.tehnokot@gmail.com>
2016-08-25 00:47:28 +0300

    fix view/book.js adnd modified fun in js/book.js

--- public/js/book.js
+++ public/js/book.js
@@ -1,11 +1,11 @@
 function fillBookInfo(book) {
     $('.bookImg img').attr('src', '/img/books/' + book.id + '.jpg');
     $('.titleBook').html(book.title);
-    $('#author').html('author: ' + book.author);
-    $('#year').html('year: ' + book.year);
-    $('#pages').html('pages: ' + book.pages);
-    $('#isbn').html('ISBN: ' + book.isbn);
-    $('.bookDescription p').html(book.description);
+    $('#author').html(book.author);
+    $('#year').html(book.year);
+    $('#pages').html(book.pages);
+    $('#isbn').html(book.isbn);
+    $('.bookDescriptionText').html(book.description);
 }
 
 doAjaxQuery('GET', '/api/v1/books/' + $(location).attr('pathname').substr(6), null, function (res) {
