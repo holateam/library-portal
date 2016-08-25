5b39733fe264ea561f26efde1497febb7eb01eeb
lex <info.tehnokot@gmail.com>
2016-08-25 13:16:50 +0300

    fix style bool.jade/css/sass/js modified gulpfile.js added func 'fonts'

--- public/js/book.js
+++ public/js/book.js
@@ -12,10 +12,10 @@ function fillBookInfo(book) {
     if(book.status=='1'){
       $('.busyBook').css('display','block');
     } else {
-      $('.busyBook').css('display','block');
+      $('.freeBook').css('display','block');
     }
 
-    
+
 }
 
 doAjaxQuery('GET', '/api/v1/books/' + $(location).attr('pathname').substr(6), null, function (res) {
@@ -24,4 +24,31 @@ doAjaxQuery('GET', '/api/v1/books/' + $(location).attr('pathname').substr(6), nu
         return;
     }
     fillBookInfo(res.data);
+});
+
+$('.orderEmail').keyup(function(event) {
+  var value = $(this).val();
+  var regex = /^[-\w.]+@([A-z0-9][-A-z0-9]+\.)+[A-z]{2,4}$/;
+  if(value == ''){
+    console.log('пусто');
+    $('.input-group').removeClass('has-error has-success');
+    view.hideElement('.glyphicon-remove');
+    view.hideElement('.glyphicon-ok');
+  }else {
+    if(regex.test(value)){
+      console.log('email true');
+      $('.input-group').removeClass('has-error has-feedback');
+      view.hideElement('.glyphicon-remove');
+
+      $('.input-group').addClass('has-success has-feedback');
+      view.showElement('.glyphicon-ok');
+    } else{
+      console.log('email false');
+      $('.input-group').removeClass('has-success has-feedback');
+      view.hideElement('.glyphicon-ok');
+      $('.input-group').addClass('has-error has-feedback');
+      view.showElement('.glyphicon-remove');
+    }
+  }
+  // alert(value);
 });
