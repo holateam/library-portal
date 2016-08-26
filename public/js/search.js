e954e75f897d2ec66d19254b9ed982b2b74c8d1d
lex <info.tehnokot@gmail.com>
2016-08-26 15:41:24 +0300

    modified search.js

--- public/js/search.js
+++ public/js/search.js
@@ -1,23 +1,31 @@
 $('#search').keyup(function(event) {
     var text = $(this).val();
     console.log(text);
-    function func() {
-        alert('Отправлен запрос на поиск текста: '+text);
-        doAjaxQuery('GET','/api/v1/books?search='+text+'',null,
-        function(res) {
+    if (text.length > 1) {
+        function func() {
+          alert('Отправлен запрос на поиск текста: ' + text);
+          doAjaxQuery('GET', '/api/v1/books?search=' + text + '', null,
+          function(res) {
             if (!res.success) {
-                alert(res.msg);
-                return;
-            }else{
+              alert(res.msg);
+              return;
+            } else {
               addBooksItems(res.data.books);
               console.log(JSON.stringify(res));
               console.log("Запрос по поиску выполнен: " + res.success);
             }
+          }
+        );
+      }
+      if (!(event.keyCode >= 33 && event.keyCode <= 40) && !(event.keyCode >= 13 && event.keyCode <= 20) && !(event.keyCode >= 16 && event.keyCode <= 20) && (event.keyCode !== 27)) {
+        var timer = setTimeout(func, 3000);
+      }
+
+      $('#search').keyup(function(event) {
+        if (!(event.keyCode >= 33 && event.keyCode <= 40) && !(event.keyCode >= 13 && event.keyCode <= 20) && !(event.keyCode >= 16 && event.keyCode <= 20))
+        {
+          clearTimeout(timer);
         }
-      );
+      });
     }
-    var timer = setTimeout(func, 3000);
-    $('#search').keyup(function(event) {
-      clearTimeout(timer);
-    });
 });
