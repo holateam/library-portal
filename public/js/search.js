9a4b4e94168e96528d33b64c81530e650547cbe8
lex <info.tehnokot@gmail.com>
2016-08-26 14:41:47 +0300

    modified search.js

--- public/js/search.js
+++ public/js/search.js
@@ -1,4 +1,22 @@
 $('#search').keyup(function(event) {
-  var text = $(this).val();
-  console.log(text);
+    var text = $(this).val();
+    console.log(text);
+    function func() {
+        alert('Отправлен запрос на поиск текста: '+text);
+        doAjaxQuery('GET','/api/v1/books?search='+text+'',null,
+        function(res) {
+            if (!res.success) {
+                alert(res.msg);
+                return;
+            }else{
+              console.log(JSON.stringify(res));
+              console.log("Запрос по поиску выполнен: " + res.success);
+            }
+        }
+      );
+    }
+    var timer = setTimeout(func, 3000);
+    $('#search').keyup(function(event) {
+      clearTimeout(timer);
+    });
 });
