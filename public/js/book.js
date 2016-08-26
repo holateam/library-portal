206e86bfb7990bc2b42fa407483606cefd6e0b4f
lex <info.tehnokot@gmail.com>
2016-08-27 01:58:49 +0300

    mini refactoring book.js

--- public/js/book.js
+++ public/js/book.js
@@ -1,57 +1,43 @@
 function fillBookInfo(book) {
-    $('.bookID').attr('book-id', book.id);
-    $('.bookImg img').attr('src', '/img/books/' + book.id + '.jpg');
-    $('.titleBook').html(book.title);
+    $('#bookID').attr('book-id', book.id);
+    $('#bookImg img').attr('src', '/img/books/' + book.id + '.jpg');
+    $('#titleBook').html(book.title);
     $('#author').html(book.author);
     $('#year').html(book.year);
     $('#pages').html(book.pages);
     $('#isbn').html(book.isbn);
-    $('.bookDescriptionText').html(book.description);
-    $('.bookID').attr('busy', book.busy);
+    $('#bookDescriptionText').html(book.description);
+    $('#bookID').attr('busy', book.busy);
 
-    console.log(JSON.stringify(book)); // log
-    console.log("Status: " + book.busy); // log
-
-    if (book.busy === true) {
-        $('.busyBook').css('display', 'block');
-    } else {
-        $('.freeBook').css('display', 'block');
-    }
-
-
+    var nameClassIsBook = (book.busy) ? '.busyBook' : '.freeBook';
+    $(nameClassIsBook).css('display', 'block');
 }
 
 doAjaxQuery('GET', '/api/v1/books/' + $(location).attr('pathname').substr(6), null, function(res) {
     if (!res.success) {
-        alert(res.msg);
-        return;
+        alert(res.msg); // to replace the normal popup
     }
     fillBookInfo(res.data);
 });
 
 /* --------------------Show the result, for sending the -----------------------
 ----------------------email in the queue for the book ---------------------- */
-var showResultSendEmailToQueue = function(email,result){
-  var busy = $('.bookID').attr('busy');
-  console.log("busy:" +busy);
-  view.hideElement('.form-queue', '.btnBookID', (busy=='true')?'.busyBook':'.freeBook');
-  view.showElement('.response');
-  $('span.youEmail').text(' '+email);
+var showResultSendEmailToQueue = function(email, result) {
+    var busy = $('#bookID').attr('busy');
+    view.hideElement('.form-queue', '.btnBookID', (busy) ? '.busyBook' : '.freeBook');
+    view.showElement('.response');
+    $('span.youEmail').text(' ' + email);
 };
 
 /*--------------- Send email. Get in Queue in for a book ---------------------*/
 var sendEmailToQueue = function(id, email) {
     doAjaxQuery('GET', '/api/v1/books/' + id + '/order?email=' + email, null, function(res) {
         if (!res.success) {
-            alert(res.msg);
-            return;
-        }else{
-          showResultSendEmailToQueue(email,res.success);
-          console.log(JSON.stringify(res));
-          console.log("Письмо отправленно " + res.success);
+            alert(res.msg); // to replace the normal popup
+        } else {
+            showResultSendEmailToQueue(email, res.success);
         }
-    }
-  );
+    });
 };
 
 /* --------------- Checking validity of email when typing in input -----------*/
@@ -59,36 +45,30 @@ $('.orderEmail').keyup(function(event) {
     var email = $(this).val();
     var isEmail = controller.validateEmail(email);
     if (email === '') {
-        console.log('пусто'); // log
         $('.input-group').removeClass('has-error has-success');
         view.hideElement('.glyphicon-remove', '.glyphicon-ok');
     } else {
         if (isEmail) {
-            console.log('email true'); // log
             view.showSuccessEmail();
             if (event.keyCode == 13) {
-              var id = $('.bookID').attr('book-id');
-              sendEmailToQueue(id,email);
-          }
+                var id = $('#bookID').attr('book-id');
+                sendEmailToQueue(id, email);
+            }
         } else {
-            console.log('email false'); // log
             view.showErrEmail();
         }
     }
-}); 
+});
 
 /*------------------ Sending email by clicking on the button ----------------*/
 $('.btnBookID').click(function(event) {
     var email = $('.orderEmail').val();
-    console.log(email);
     var isEmail = controller.validateEmail(email);
-    if(isEmail){
-      view.showSuccessEmail();
-      var id = $('.bookID').attr('book-id');
-      console.log('Click Btn ');
-      console.log(id);
-      sendEmailToQueue(id, email);
-    }else{
-      view.showErrEmail();
+    if (isEmail) {
+        view.showSuccessEmail();
+        var id = $('#bookID').attr('book-id');
+        sendEmailToQueue(id, email);
+    } else {
+        view.showErrEmail();
     }
 });
