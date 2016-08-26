8afb950304fd00ed4af9b1b24a463b04843f31bc
lex <info.tehnokot@gmail.com>
2016-08-26 19:40:22 +0300

    reduced the length of the word for auto-search

--- public/js/search.js
+++ public/js/search.js
@@ -1,6 +1,7 @@
 var requestBooksSearch = function() {
     var text = $('#search').val();
-    doAjaxQuery('GET', '/api/v1/books?search=' + text + '', null,
+    console.log(encodeURIComponent(text));
+    doAjaxQuery('GET', '/api/v1/books?search=' + encodeURIComponent(text) + '', null,
         function(res) {
             if (!res.success) {
                 alert(res.msg);
@@ -19,7 +20,7 @@ $('#search').keydown(function(event) {
         requestBooksSearch();
     }
     var text = $(this).val();
-    if (text.length > 1) {
+    if (text.length > 0) {
         if (!(event.keyCode >= 33 && event.keyCode <= 40) && !(event.keyCode >= 13 && event.keyCode <= 20) && !(event.keyCode >= 16 && event.keyCode <= 20) && (event.keyCode !== 27) && (event.keyCode !== 13)) {
             var task = setTimeout(requestBooksSearch, 2000);
         }
