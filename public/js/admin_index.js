24eaa2655c5343d6c1d7aafd8784b6625be4bd90
serhii-kobzin <serhii-kobzin@gmail.com>
2016-08-27 19:44:03 +0300

    fix admin_index.js

--- public/js/admin_index.js
+++ public/js/admin_index.js
@@ -1,13 +1,13 @@
 function addBookListRow(book) {
     $('#pattern').clone().removeAttr('id').attr('book-id', book.id)
         .html('<td>' + book.title + '</td><td>' + book.author + '</td><td>' + nullToString(book.name) + '</td><td>' +
-            nullToString(book.phone) + '</td><td>' + nullToString(book.email) + '</td><td>' + nullToString(book.term) +
+            nullToString(book.email) + '</td><td>' + nullToString(book.phone) + '</td><td>' + nullToString(book.term) +
             '</td><td>' + nullToString(book.pawn) + '</td><td>' + nullToString(book.status) + '</td>')
         .css('display', 'table-row').appendTo('.table tbody');
 }
 
 function nullToString(string) {
-    return (((string == null) || (string == 0)) ? '-' : null);
+    return (((string == null) || (string == 0)) ? '-' : string);
 }
 
 function addBooksList(books) {
