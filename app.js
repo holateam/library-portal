8cfe18a5db9fe0f0539e5b0bbeb873b700f046b8
Dmitriy Malyarenko <dmitriy.malyarenko@gmail.com>
2016-08-28 01:39:00 +0300

    modified app.js

--- app.js
+++ app.js
@@ -7,7 +7,6 @@ var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
-var book = require('./routes/book');
 var adminRouter = require('./routes/admins');
 var booksRouter = require('./routes/books');
 
