f8e616393a42a5a85d0f16e9f2983c3c5acfbc4b
serhii-kobzin <serhii-kobzin@gmail.com>
2016-08-30 03:14:43 +0300

    fix magic numbers in admin_index.js

--- public/js/admin_index.js
+++ public/js/admin_index.js
@@ -1,8 +1,9 @@
 var search = $(location).attr('search');
+var stringToFind = '?filter=';
+var stringPosition = search.indexOf(stringToFind);
 
-if (search) {
-    var filterPosition = search.indexOf('=') + 1;
-    $('.sidebar_item[data-filter=' + search.substr(filterPosition) + ']').click();
+if (stringPosition == 0) {
+    $('.sidebar_item[data-filter=' + search.substr(stringToFind.length) + ']').click();
 } else {
     $('.sidebar_item[data-filter=all]').click();
 }
