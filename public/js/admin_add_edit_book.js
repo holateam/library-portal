e88780af69728c4dcd7cd2c23f12bfee40bdcc97
shpp-krusso <kstn.russo@gmail.com>
2016-09-01 00:43:40 +0300

    add image cropper and resizer without transferring'

--- public/js/admin_add_edit_book.js
+++ public/js/admin_add_edit_book.js
@@ -7,29 +7,35 @@ function fillBookEditor(book) {
     $('#book_pages').val(book.pages);
     $('#book_isbn').val(book.isbn);
     $('#book_description').val(book.description);
-    $("#book_img_upload").fileinput({
-        initialPreview: [
-               '/img/books/' + book.id + '.jpg',
-           ],
-           initialPreviewAsData: true,
-           initialPreviewConfig: [
-                {caption: ""+book.id+".jpg", width: "120px", key: 1, showDelete: false}
-            ],
-           overwriteInitial: true,
-           initialCaption: ""+book.id+".jpg",
-        allowedFileExtensions: ["jpg", "png", "gif"],
-        minImageWidth: 50,
-        minImageHeight: 50
-    });
+
+    // $("#book_img_upload").fileinput({
+    //     initialPreview: [
+    //            '/img/books/' + book.id + '.jpg',
+    //        ],
+    //        initialPreviewAsData: true,
+    //        initialPreviewConfig: [
+    //             {caption: ""+book.id+".jpg", width: "120px", key: 1, showDelete: false}
+    //         ],
+    //        overwriteInitial: true,
+    //        initialCaption: ""+book.id+".jpg",
+    //     allowedFileExtensions: ["jpg", "png", "gif"],
+    //     minImageWidth: 50,
+    //     minImageHeight: 50
+    // });
 
     // $('.kv-file-content img').attr('src', '/img/books/' + book.id + '.jpg');
 }
 
-$('#book_img_upload').change(function() {
+var canvas = document.getElementById('canvas');
+var context = canvas.getContext('2d');
+
+$('#book_img_upload').change(function () {
+    context.clearRect(0, 0, canvas.width, canvas.height);
+
     var file = $(this)[0].files[0];
     var fileReader = new FileReader();
 
-    fileReader.onload = function() {
+    fileReader.onload = function () {
         fileInBase64 = fileReader.result;
         $('#book_img').attr('src', fileInBase64);
     };
@@ -42,21 +48,78 @@ var stringToFind = '/admin/book/update/';
 var stringPosition = pathname.indexOf(stringToFind);
 
 if (stringPosition == 0) {
-    doAjaxQuery('GET', '/admin/api/v1/books/' + pathname.substr(stringToFind.length), null, function(res) {
+    doAjaxQuery('GET', '/admin/api/v1/books/' + pathname.substr(stringToFind.length), null, function (res) {
         if (!res.success) {
             alert(res.msg); // to replace the normal popup
         }
         fillBookEditor(res.data);
     });
-}else {
-  $("#book_img_upload").fileinput({
-     allowedFileExtensions: ["jpg", "png", "gif"],
-     minImageWidth: 50,
-     minImageHeight: 50
- });
+} else {
+
+    var imageObj = new Image();
+    $('#book_img_upload').change(function (e) {
+
+        function readURL(input) {
+            if (input.files[0]) {
+                var reader = new FileReader();
+                reader.onload = function (e) {
+                    // console.log(e.target.result);
+                    // $('#imgInp').attr('src', e.target.result);
+                    imageObj.src = e.target.result;
+                };
+                reader.readAsDataURL(input.files[0]);
+            }
+        }
+
+        readURL(e.target);
+
+        imageObj.onload = function () {
+
+            $('#imgInp').hide();
+            var sourceX = 0,
+                sourceY = 0,
+                sourceWidth = imageObj.width,
+                sourceHeight = imageObj.height,
+                destWidth,
+                destHeight,
+                destX,
+                destY = null;
+
+
+            if (imageObj.width > imageObj.height) {
+
+                destWidth = canvas.width;
+                destHeight = (sourceHeight * canvas.width) / sourceWidth;
+                destX = 0;
+                destY = (canvas.height - destHeight) / 2;
+
+            } else {
+
+                destWidth = (sourceWidth * canvas.height) / sourceHeight;
+                destHeight = canvas.height;
+                destX = (canvas.width - destWidth) / 2;
+                destY = 0;
+            }
+
+            context.drawImage(imageObj, sourceX, sourceY, sourceWidth, sourceHeight, destX, destY, destWidth, destHeight);
+        };
+    });
+
+
+
+    //         //  $("#book_img_upload").fileinput({
+    //         //     allowedFileExtensions: ["jpg", "png", "gif"],
+    //         //     minImageWidth: 50,
+    //         //     minImageHeight: 50
+    //         // });
+    //     // });
+    // });
 }
 
 $('#book_save').click(function () {
+    var dataURL = canvas.toDataURL("image/png"),
+        s = dataURL.replace(/^data:image\/png;base64,/, "");
+    console.log(s);
     var data = {
         changes: {
             title: $('#book_title').val(),
@@ -65,15 +128,28 @@ $('#book_save').click(function () {
             pages: $('#book_pages').val(),
             isbn: $('#book_isbn').val(),
             description: $('#book_description').val(),
-            img: fileInBase64
+            img: s
         }
     };
-    doAjaxQuery('POST', '/admin/api/v1/books/' + ((stringPosition == 0) ? 'update/' : 'add/') + pathname.substr(stringToFind.length), data, function(res) {
-        if (!res.success) {
-            alert(res.msg); // to replace the normal popup
-            return;
+    // doAjaxQuery('POST', '/admin/api/v1/books/' + ((stringPosition == 0) ? 'update/' : 'add/') + pathname.substr(stringToFind.length), data, function (res) {
+    //     if (!res.success) {
+    //         alert(res.msg); // to replace the normal popup
+    //         return;
+    //     }
+    //     alert('Success');
+    // });
+
+    $.ajax({
+        url: '/admin/api/v1/books/add',
+        dataType: 'json',
+        contentType: 'application/json',
+        data: JSON.stringify({file: s}),
+        type: 'POST',
+        success: function (data, err) {
+            if (err) {
+                console.log(err);
+            }
         }
-        alert('Success');
     });
 });
 
