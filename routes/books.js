1b3bddf13a8245405a105de34b26fcafc8ea8a52
shpp-krusso <kstn.russo@gmail.com>
2016-09-05 17:57:55 +0300

    russification

--- routes/books.js
+++ routes/books.js
@@ -5,46 +5,46 @@ var verify = require('../verify.js');
 var dbLayer = require('../models/DB_MYSQL.js');
 
 booksRouter.route('/')
-.get(function(req, res, next) {
-    var data = {};
-    data.filter = req.query.filter;
-    data.limit = req.query.limit;
-    data.offset = req.query.offset;
-    data.search = req.query.search;
+    .get(function (req, res, next) {
+        var data = {};
+        data.filter = req.query.filter;
+        data.limit = req.query.limit;
+        data.offset = req.query.offset;
+        data.search = req.query.search;
 
-    dbLayer.getBooks(data, function(err, resp) {
-        if (err) {
-            res.json({ success: false, msg: err });
-        } else {
-            res.json({ success: true, data: resp});
-        }
+        dbLayer.getBooks(data, function (err, resp) {
+            if (err) {
+                res.json({success: false, msg: err});
+            } else {
+                res.json({success: true, data: resp});
+            }
+        });
     });
-});
+
 
 booksRouter.route('/:book_id')
-.get(function(req, res, next) {
-
-    dbLayer.getBook(req.params.book_id, function(err, resp) {
-        if (err) {
-            res.json({ success: false, msg: err });
-        } else {
-            res.json({ success: true, data: resp});
-        }
+    .get(function (req, res, next) {
+        dbLayer.getBook(req.params.book_id, function (err, resp) {
+            if (err) {
+                res.json({success: false, msg: err});
+            } else {
+                res.json({success: true, data: resp});
+            }
+        });
     });
-});
 
 booksRouter.route('/:book_id/order')
-.get(function(req, res, next) {
-    var data = {};
-    data.book_id = req.params.book_id;
-    data.email = req.query.email;
-    dbLayer.addToQueue(data, function(err, resp) {
-        if (err) {
-            res.json({ success: false, msg: err });
-        } else {
-            res.json({ success: true});
-        }
+    .get(function (req, res, next) {
+        var data = {};
+        data.book_id = req.params.book_id;
+        data.email = req.query.email;
+        dbLayer.addToQueue(data, function (err, resp) {
+            if (err) {
+                res.json({success: false, msg: err});
+            } else {
+                res.json({success: true});
+            }
+        });
     });
-});
 
 module.exports = booksRouter;
