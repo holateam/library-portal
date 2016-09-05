b1b125996e747060fcec586f3fd0c759eda539de
Dmitriy Malyarenko <dmitriy.malyarenko@gmail.com>
2016-09-05 19:20:32 +0300

    modified routes/books.js

--- routes/books.js
+++ routes/books.js
@@ -4,47 +4,29 @@ var booksRouter = express.Router();
 var verify = require('../verify.js');
 var dbLayer = require('../models/DB_MYSQL.js');
 
+var standardRes = function(err, resp) {
+    return err ? { success: !err, msg: err } : { success: !err, data: resp };
+};
+
 booksRouter.route('/')
-    .get(function (req, res, next) {
-        var data = {};
-        data.filter = req.query.filter;
-        data.limit = req.query.limit;
-        data.offset = req.query.offset;
-        data.search = req.query.search;
-
-        dbLayer.getBooks(data, function (err, resp) {
-            if (err) {
-                res.json({success: false, msg: err});
-            } else {
-                res.json({success: true, data: resp});
-            }
-        });
+.get(function(req, res, next) {
+    dbLayer.getBooks(req.query, function(err, resp) {
+        res.json(standardRes(err, resp));
     });
-
+});
 
 booksRouter.route('/:book_id')
-    .get(function (req, res, next) {
-        dbLayer.getBook(req.params.book_id, function (err, resp) {
-            if (err) {
-                res.json({success: false, msg: err});
-            } else {
-                res.json({success: true, data: resp});
-            }
-        });
+.get(function(req, res, next) {
+    dbLayer.getBook(req.params.book_id, function(err, resp) {
+        res.json(standardRes(err, resp));
     });
+});
 
 booksRouter.route('/:book_id/order')
-    .get(function (req, res, next) {
-        var data = {};
-        data.book_id = req.params.book_id;
-        data.email = req.query.email;
-        dbLayer.addToQueue(data, function (err, resp) {
-            if (err) {
-                res.json({success: false, msg: err});
-            } else {
-                res.json({success: true});
-            }
-        });
+.get(function(req, res, next) {
+    dbLayer.addToQueue(req.params.book_id, req.query, function(err, resp) {
+        res.json(standardRes(err, resp));
     });
+});
 
 module.exports = booksRouter;
