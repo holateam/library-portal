9eb1543dfc87b3f7a0c0fc45559cfe600fc5736d
shpp-achudinovskyh <andron989@gmail.com>
2016-09-06 17:07:12 +0300

    mailer fix

--- models/mailer.js
+++ models/mailer.js
@@ -4,27 +4,35 @@ var nodemailer = require('nodemailer');
 var transporter = nodemailer.createTransport();
 
 module.exports.sendMail = function (book_id) {
-  db.getQueueByBookId(book_id, function (err, result) {
-    if(err) throw err;
-      var mailOptions={
-              from: 'noreply@programming.kr.ua',
-              subject: 'Library SH++',
-              html: '<b>The book with id= '+ book_id +' is free!</b>',
-              text: 'The book with id='+ book_id +' is free!'
-      };
-      for(var i = 0; i < result.length; i++){
-          mailOptions.to = result[i].email;
-          transporter.sendMail(mailOptions, function(error, info){
-              if(error){
-                  console.log(error);
-              }
-              db.deleteFromQueue(book_id,mailOptions.to,function (err, result) {
-                  if(err){
-                      console.log(err);
-                  }
-                  console.log(result);
-              })
-          });
-      }
-  });
+    db.getBook(book_id,function (err, bookInfo) {
+        if (err){
+            return console.log(err);
+        }
+        db.getQueueByBookId(bookInfo.id, function (err, result) {
+            if (err){
+                return console.log(err);
+            }
+
+            var mailOptions={
+                from: 'noreply@programming.kr.ua',
+                subject: 'Library SH++',
+                html: '<b>Книга '+ bookInfo.title +' автор: ' + bookInfo.author + ', на которую вы подписались, теперь свободна.</b>',
+                text: 'Книга '+ bookInfo.title +' автор: ' + bookInfo.author + ', на которую вы подписались, теперь свободна.'
+            };
+            for(var i = 0; i < result.length; i++){
+                mailOptions.to = result[i].email;
+                transporter.sendMail(mailOptions, function(error, info){
+                    if(error){
+                        console.log(error);
+                    }
+                    db.deleteFromQueue(book_id,mailOptions.to,function (err, result) {
+                        if(err){
+                            console.log(err);
+                        }
+                        console.log(result);
+                    })
+                });
+            }
+        });
+    });
 };
