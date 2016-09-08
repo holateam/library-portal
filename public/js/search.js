ade602ebc338476d15a3c4a09bb61025c7aae754
lex <info.tehnokot@gmail.com>
2016-09-08 17:57:33 +0300

    fix setTimeout in search

--- public/js/search.js
+++ public/js/search.js
@@ -36,7 +36,7 @@ var callbackQueryItemsSearch = function(res, text) {
 /* ------------------- Get the query in database searching -------------------*/
 var requestBooksSearch = function(callback) {
     var text = $('#search').val();
-
+    console.log('текст для поиска: ' + text);
     if (text.length > 0) {
         text = text.replace(/(^\s+|\s+$)/g, '');
         var textEncode = encodeURIComponent(text); // shielding request
@@ -72,6 +72,7 @@ if (pathNameUrl[1] == 'search') {
 /* ---------- Live search if the search did not introduced n time ----------- */
 $('#search').keydown(function(event) {
     var text = $(this).val();
+    console.log("текст: " + text + " длина: " + text.length);
     if (event.keyCode === 13) {
         event.preventDefault();
         if (text.length > 0) {
@@ -95,14 +96,19 @@ $('#search').keydown(function(event) {
             !(event.keyCode >= 16 && event.keyCode <= 20) &&
             (event.keyCode !== 27) &&
             (event.keyCode !== 13)) {
-            var task = setTimeout(requestBooksSearch(callbackQueryMiniItemsSearch), 500);
+            var task = setTimeout(function() {
+                requestBooksSearch(callbackQueryMiniItemsSearch);
+            }, 500);
         }
 
         $('#search').keydown(function(event) {
             if (!(event.keyCode >= 33 && event.keyCode <= 40) &&
                 !(event.keyCode >= 16 && event.keyCode <= 20)) {
+                console.log('Отмена');
                 clearTimeout(task);
             }
         });
+    } else {
+        $('#list').hide();
     }
 });
